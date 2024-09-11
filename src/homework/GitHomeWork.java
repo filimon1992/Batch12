8aebdf0682cbf529cda81143e400ba4a82c5963e
filiG <filiG@108home.mynetworksettings.com>
2024-09-10 21:16:13 -0400

    werwgv

--- src/homework/GitHomeWork.java
+++ src/homework/GitHomeWork.java
@@ -2,29 +2,5 @@ package homework;
 
 public class GitHomeWork {
 
-	public static void main (String [] args) {
-		System.out.println("BATCH 12: Git Class Lecture");
-		int age =12;
-		String name="jhon";
-		//this is coming from github
-	}
-	public static void add (int a, int b) {
-		System.out.println(a+b);
-	int age=24;
-		String name="fili";
-}
-	public static void subtract (int a, int b) {
-		System.out.println(a-b);
-		int age=32;
-		String name="zedu";
-			
-}
-public static void divide  (int a, int b) {
-		System.out.println(a/b);
-}	
-		public static void  multipied(int a, int b) {
-			
-			System.out.println(a%b);
-			
-	}
+
 }
