b604f071c4933af1472005163978b1dbb71bc756
filimon1992 <guess_god@yahoo.com>
2024-09-10 20:50:15 -0400

    Update GitHomeWork.java

--- src/homework/GitHomeWork.java
+++ src/homework/GitHomeWork.java
@@ -4,6 +4,9 @@ public class GitHomeWork {
 
 	public static void main (String [] args) {
 		System.out.println("BATCH 12: Git Class Lecture");
+		int age =12;
+		String name="jhon";
+		//this is coming from github
 	}
 	public static void add (int a, int b) {
 		System.out.println(a+b);
