47393d115def0309c4356a56a86cebfe15c276be
filiG <filiG@108home.mynetworksettings.com>
2024-09-10 20:36:57 -0400

    added new method

--- src/homework/GitHomeWork.java
+++ src/homework/GitHomeWork.java
@@ -5,7 +5,8 @@ public class GitHomeWork {
 	public static void main (String [] args) {
 		System.out.println("BATCH 12: Git Class Lecture");
 	}
+	public static void add (int a, int b) {
+		System.out.println(a+b);
 	
-	
-	
+}
 }
